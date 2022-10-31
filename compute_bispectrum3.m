0901fdb2213a5650215507189e29bd9b67d8c0f9
liping1005 <80134911+liping1005@users.noreply.github.com>
2022-10-31 18:35:48 -0400

    Delete compute_bispectrum3.m

--- compute_bispectrum3.m
+++
@@ -1,8 +0,0 @@
-function [Bf] = compute_bispectrum3(f_hat)
-%Input: FT of a signal
-%Output: Bispectrum of the signal (according to Singer definition)
-Bf1 = (f_hat.')*conj(f_hat);
-Bf2 = circulant(f_hat);
-Bf = Bf1.*Bf2;
-end
-
