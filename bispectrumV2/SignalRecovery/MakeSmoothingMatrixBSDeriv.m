7f07b63581f26995eaad633a27a39859a40018df
liping1005 <80134911+liping1005@users.noreply.github.com>
2023-05-04 22:07:30 -0400

    Delete bispectrumV2/SignalRecovery directory

--- bispectrumV2/SignalRecovery/MakeSmoothingMatrixBSDeriv.m
+++
@@ -1,9 +0,0 @@
-%https://bitbucket.org/annavlittle/inversion-unbiasing/src/master/SupportingFunctions/MakeSmoothingMatrix.m
-function SmoothingMatrix = MakeSmoothingMatrixBSDeriv(w, width)
-    SmoothingMatrix = zeros(length(w),length(w));
-    %width = 10;
-    for i=1:length(w)
-        for j=1:length(w)
-            SmoothingMatrix(i,j) = -(1/(4*pi*width^4))*(w(i)^2 + w(j)^2)^(1/2) * exp( (-w(i)^2 - w(j)^2) / (2*width^2) );
-        end
-    end
